f875f3b25b5d485759c9e36be8f1bac277b6ef80
wangzhensuo <wangqiang1010@163.com>
2017-11-07 13:26:25 +0800

    update
    实现爬取51job,多线程，保存到MySQL数据库。

--- wxBot/其他无关代码/51job.py
+++ wxBot/其他无关代码/51job.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8
 import re
-import urllib.request
-import bs4
-import sys
-import json
+import pymysql
 import csv
 import time
 import requests
@@ -17,73 +14,6 @@ user_agent_list = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
     "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
     "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
-    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; Media Center PC 6.0; InfoPath.2; MS-RTC LM 8)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0 Zune 3.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; MS-RTC LM 8)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET CLR 4.0.20402; MS-RTC LM 8)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET CLR 1.1.4322; InfoPath.2)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Tablet PC 2.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET CLR 3.0.04506; Media Center PC 5.0; SLCC1)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Tablet PC 2.0; .NET CLR 3.0.04506; Media Center PC 5.0; SLCC1)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; FDM; Tablet PC 2.0; .NET CLR 4.0.20506; OfficeLiveConnector.1.4; OfficeLivePatch.1.3)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET CLR 3.0.04506; Media Center PC 5.0; SLCC1; Tablet PC 2.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET CLR 1.1.4322; InfoPath.2)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.3029; Media Center PC 6.0; Tablet PC 2.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; Media Center PC 3.0; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.1)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; FDM; .NET CLR 1.1.4322)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; InfoPath.1; .NET CLR 2.0.50727)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; InfoPath.1)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; Alexa Toolbar; .NET CLR 2.0.50727)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; Alexa Toolbar)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.40607)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322)',
-    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.0.3705; Media Center PC 3.1; Alexa Toolbar; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-    'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
-    'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; el-GR)',
-    'Mozilla/5.0 (MSIE 7.0; Macintosh; U; SunOS; X11; gu; SV1; InfoPath.2; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648)',
-    'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; c .NET CLR 3.0.04506; .NET CLR 3.5.30707; InfoPath.1; el-GR)',
-    'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; c .NET CLR 3.0.04506; .NET CLR 3.5.30707; InfoPath.1; el-GR)',
-    'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; fr-FR)',
-    'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; en-US)',
-    'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 5.2; WOW64; .NET CLR 2.0.50727)',
-    'Mozilla/4.79 [en] (compatible; MSIE 7.0; Windows NT 5.0; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648)',
-    'Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)',
-    'Mozilla/4.0 (Mozilla/4.0; MSIE 7.0; Windows NT 5.1; FDM; SV1; .NET CLR 3.0.04506.30)',
-    'Mozilla/4.0 (Mozilla/4.0; MSIE 7.0; Windows NT 5.1; FDM; SV1)',
-    'Mozilla/4.0 (compatible;MSIE 7.0;Windows NT 6.0)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0;)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; YPC 3.2.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; InfoPath.2; .NET CLR 3.5.30729; .NET CLR 3.0.30618)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; YPC 3.2.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; Media Center PC 5.0; .NET CLR 2.0.50727)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 3.0.04506)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; InfoPath.2; .NET CLR 3.5.30729; .NET CLR 3.0.30618; .NET CLR 1.1.4322)',
 ]
 # header = {
@@ -94,7 +24,7 @@ user_agent_list = [
 data_count =0
 def get_index_url(url):
     header = {
-        'User-Agent': user_agent_list[random.randint(1,59)],
+        'User-Agent': user_agent_list[random.randint(0,5)],
         'Connection': 'keep-alive',
     }
 
@@ -104,68 +34,86 @@ def get_index_url(url):
     time.sleep(1)
     for link in links:
         try:
+            db = pymysql.connect(host='localhost', port=3307, user='root', password='usbw', db='test', charset='utf8')
+            cur = db.cursor()
             global data_count
             data_count = data_count + 1
             print('第' + str(data_count) + '子页面')
             page_url = link.get('href')
             wbdata2 = requests.get(page_url, headers=header).content
-            soup2 = BeautifulSoup(wbdata2, 'html.parser',
-                                  from_encoding="GBK")
+            soup2 = BeautifulSoup(wbdata2, 'html.parser',from_encoding="GBK")
             job_name = "NONE"
             job_name = soup2.select(
-                'html > body > div.tCompanyPage > div.tCompany_center.clearfix > div.tHeader.tHjob > div.in > div.cn > h1')[0].text.replace(' ', '')
-            # print(job_name)
-            # print('-------1--------')
+                'html > body > div.tCompanyPage > div.tCompany_center.clearfix > div.tHeader.tHjob > div.in > div.cn > h1')[0].text.strip().replace("\n", "").replace(' ', '').replace("\t", "").replace("\r", "").replace(",",".").replace('\'','')
+
             gongzi = "NONE"
             gongzi = soup2.select(
                 'html > body > div.tCompanyPage > div.tCompany_center.clearfix > div.tHeader.tHjob > div > div.cn > strong')[0].text
             jieshao = "NONE"
             jieshao = soup2.select(
-                'html > body > div.tCompanyPage > div.tCompany_center.clearfix > div.tCompany_main > div.tBorderTop_box > div.bmsg.job_msg.inbox')[0].text.strip().replace("\n", "").replace(' ', '').replace("\t", "").replace("\r", "")
-            # print(jieshao)
-            # print('-----2----------')
-            company_name='NONE'
-            company_name = soup2.select('body > div.tCompanyPage > div.tCompany_center.clearfix > div.tHeader.tHjob > div > div.cn > p.cname > a')[0].text.replace("\n", "").replace("\r", "")
+                'html > body > div.tCompanyPage > div.tCompany_center.clearfix > div.tCompany_main > div.tBorderTop_box > div.bmsg.job_msg.inbox')[0].text.strip().replace("\n", "").replace(' ', '').replace("\t", "").replace("\r", "").replace(",",".")
 
-            # print('------3---------')
+            company_name='NONE'
+            company_name = soup2.select('body > div.tCompanyPage > div.tCompany_center.clearfix > div.tHeader.tHjob > div > div.cn > p.cname > a')[0].text.strip().replace("\n", "").replace(' ', '').replace("\t", "").replace("\r", "").replace(",",".")
+
             company_addr='NONE'
-                                               #body > div.tCompanyPage > div.tCompany_center.clearfix > div.tCompany_main > div:nth-child(3) > div > p
-            company_addr = soup2.select('.fp')[-1].text.replace("\n", "").replace("\r", "")
-            # print(company_addr)
-            # print('------4---------')
+            #body > div.tCompanyPage > div.tCompany_center.clearfix > div.tCompany_main > div:nth-child(3) > div > p
+            company_addr = soup2.select('.fp')[-1].text.strip().replace("\n", "").replace(' ', '').replace("\t", "").replace("\r", "").replace(",",".")
         except:
             print("not found!"+page_url,company_name,company_addr,gongzi)
             with open("C:/log.csv", 'a', encoding='utf-8') as f:
-                f.write("not found!"+','+page_url+','+company_name+','+company_addr+','+gongzi)
+                f.write("not found!"+','+page_url+','+company_name+','+company_addr+','+gongzi+'\n')
             pass
-        with open("C:/51data.csv", 'a', encoding='utf-8') as f:
-            try:
-                # print('---------w--')
-                f.write(job_name)
-                f.write(',')
-                f.write(company_name)
-                f.write(',')
-                f.write(company_addr)
-                f.write(',')
-                gongzi_p = str2float(gongzi)
-                f.write(str(gongzi_p[0]))
-                f.write(',')
-                f.write(str(gongzi_p[1]))
-                f.write(',')
-                f.write(page_url)
-                f.write(',')
-                f.write(jieshao)
-                f.write(',' + "\n")
 
-            except:
-                print("error!!!")
-                print((job_name, gongzi, page_url, url, jieshao))
-                pass
+        print('----------1-----')
+        gongzi_p = str2float(gongzi)
+        gongzi_min=str(gongzi_p[0])
+        gongzi_max = str(gongzi_p[1])
+        job_url = page_url
+        job_info = jieshao.replace('\'','')
+        print('------2---------')
+        data_id=re.findall("\d+",job_url)[1]
+        print(data_id)
+        sql_insert = """insert into dalianjob(data_id,job_name,company_name,company_addr,gongzi_min,gongzi_max,job_url,job_info) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')""".format(
+            data_id,job_name, company_name, company_addr, gongzi_min, gongzi_max, job_url, job_info)
+        print('------3---------')
+        # time.sleep(1)
+        cur.execute(sql_insert)
+        print('------4---------')
+        print(job_name,company_name,company_addr,gongzi_min,gongzi_max,job_url,job_info)
+        db.commit()
+        print('------5---------')
+        cur.close()
+        db.close()
+        print('------6---------')
+        # with open("C:/data.csv", 'a', encoding='utf-8') as f:
+        #     try:
+        #         # print('---------w--')
+        #         f.write(job_name.replace(",","."))
+        #         f.write(',')
+        #         f.write(company_name.replace(",","."))
+        #         f.write(',')
+        #         f.write(company_addr.replace(",","."))
+        #         f.write(',')
+        #         gongzi_p = str2float(gongzi)
+        #         f.write(str(gongzi_p[0]))
+        #         f.write(',')
+        #         f.write(str(gongzi_p[1]))
+        #         f.write(',')
+        #         f.write(page_url.replace(",","."))
+        #         f.write(',')
+        #         f.write(jieshao.replace(",","."))
+        #         f.write("\n")
+
+            # except:
+            #     print("error!!!")
+            #     print((job_name, gongzi, page_url, url, jieshao))
+            #     pass
 
 def str2float(str_data):
     # str_data="2.5-3千/月"
     gz = re.findall("(\d.*\d*)-(\d.*\d*)千", str_data)
-    if not gz:#如果找不
+    if not gz:
         min_gz = 0
         max_gz = 0
         gz_w = re.findall("(\d.*\d*)-(\d.*\d*)万", str_data)
@@ -173,7 +121,6 @@ def str2float(str_data):
             min_gz = 0
             max_gz = 0
         else:
-            # print(gz_w)
             min_gz = float(gz_w[0][0]) * 10000
             max_gz = float(gz_w[0][1]) * 10000
     else:
@@ -186,7 +133,6 @@ if __name__ == "__main__":
     try:
         pool = multiprocessing.Pool(processes=8)
         for i in range(1, 1105):
-            # print('第'+str(i)+'大页')
             url = "http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=230300%2C00&district=000000&funtype=0000&industrytype=00&issuedate=9&providesalary=99&keywordtype=2&curr_page=" + str(
                 i)
             pool.apply_async(get_index_url, (url,))  # 维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
